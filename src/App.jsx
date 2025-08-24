b73938b771b0b50fc3e9ac055cf15bfe614eb631
Philzyyyy <philipp.zybala@gmx.de>
2025-08-24 14:17:40 +0200

    TalwindCSS & Files aufgeräumt

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>Hello Quiz-App!</h1>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Hello Quiz App 🚀</h1>
     </div>
   );
 }
-
-export default App;
